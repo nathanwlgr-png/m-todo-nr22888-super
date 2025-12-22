3335bad9a8ab1722e93f5e9e7f5443792d0a2676
Nathan Rosa <nathan.wlgr@gmail.com>
2025-12-22 05:39:36 +0000

    File changes

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -39,12 +39,18 @@ const decisionRoles = [
   { value: 'socio', label: 'Sócio' },
 ];
 
-// Numerology calculation
+// Numerologia Pitagórica
 const calculateNumerology = (name) => {
   const values = {
-    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9,
-    j: 1, k: 2, l: 3, m: 4, n: 5, o: 6, p: 7, q: 8, r: 9,
-    s: 1, t: 2, u: 3, v: 4, w: 5, x: 6, y: 7, z: 8
+    a: 1, j: 1, s: 1,
+    b: 2, k: 2, t: 2,
+    c: 3, l: 3, u: 3,
+    d: 4, m: 4, v: 4,
+    e: 5, n: 5, w: 5,
+    f: 6, o: 6, x: 6,
+    g: 7, p: 7, y: 7,
+    h: 8, q: 8, z: 8,
+    i: 9, r: 9
   };
   
   let sum = 0;
