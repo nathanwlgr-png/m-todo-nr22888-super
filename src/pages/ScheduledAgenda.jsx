a13e200cfec55bc3446442ac97344c471a4fd6c0
Nathan Rosa <nathan.wlgr@gmail.com>
2025-12-22 14:57:51 +0000

    File changes

--- src/pages/ScheduledAgenda.jsx
+++ src/pages/ScheduledAgenda.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
