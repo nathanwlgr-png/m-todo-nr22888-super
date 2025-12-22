5331e9c58afe98bfe947c1f4411bdb5054abdc9d
Nathan Rosa <nathan.wlgr@gmail.com>
2025-12-22 11:31:25 +0000

    Update main page to MyProfile

--- src/pages.config.js
+++ src/pages.config.js
@@ -5,8 +5,10 @@ import CaptureLeads from './pages/CaptureLeads';
 import ClientProfile from './pages/ClientProfile';
 import Clients from './pages/Clients';
 import ClientsByCity from './pages/ClientsByCity';
+import ConsumablePriceList from './pages/ConsumablePriceList';
 import CreateFollowUpSequence from './pages/CreateFollowUpSequence';
 import Equipment from './pages/Equipment';
+import EquipmentPriceList from './pages/EquipmentPriceList';
 import FollowUpAssistant from './pages/FollowUpAssistant';
 import FollowUpSequences from './pages/FollowUpSequences';
 import GlobalSearch from './pages/GlobalSearch';
@@ -14,6 +16,7 @@ import Goals from './pages/Goals';
 import GoogleCalendarSettings from './pages/GoogleCalendarSettings';
 import Home from './pages/Home';
 import ImportLeads from './pages/ImportLeads';
+import ImportPriceList from './pages/ImportPriceList';
 import LeadProfile from './pages/LeadProfile';
 import Leaderboard from './pages/Leaderboard';
 import Leads from './pages/Leads';
@@ -35,9 +38,6 @@ import TaskCalendar from './pages/TaskCalendar';
 import Tasks from './pages/Tasks';
 import VisitSummary from './pages/VisitSummary';
 import WhatsAppInbox from './pages/WhatsAppInbox';
-import ImportPriceList from './pages/ImportPriceList';
-import EquipmentPriceList from './pages/EquipmentPriceList';
-import ConsumablePriceList from './pages/ConsumablePriceList';
 import __Layout from './Layout.jsx';
 
 
@@ -49,8 +49,10 @@ export const PAGES = {
     "ClientProfile": ClientProfile,
     "Clients": Clients,
     "ClientsByCity": ClientsByCity,
+    "ConsumablePriceList": ConsumablePriceList,
     "CreateFollowUpSequence": CreateFollowUpSequence,
     "Equipment": Equipment,
+    "EquipmentPriceList": EquipmentPriceList,
     "FollowUpAssistant": FollowUpAssistant,
     "FollowUpSequences": FollowUpSequences,
     "GlobalSearch": GlobalSearch,
@@ -58,6 +60,7 @@ export const PAGES = {
     "GoogleCalendarSettings": GoogleCalendarSettings,
     "Home": Home,
     "ImportLeads": ImportLeads,
+    "ImportPriceList": ImportPriceList,
     "LeadProfile": LeadProfile,
     "Leaderboard": Leaderboard,
     "Leads": Leads,
@@ -79,13 +82,10 @@ export const PAGES = {
     "Tasks": Tasks,
     "VisitSummary": VisitSummary,
     "WhatsAppInbox": WhatsAppInbox,
-    "ImportPriceList": ImportPriceList,
-    "EquipmentPriceList": EquipmentPriceList,
-    "ConsumablePriceList": ConsumablePriceList,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "MyProfile",
     Pages: PAGES,
     Layout: __Layout,
 };
