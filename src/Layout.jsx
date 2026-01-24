110fa6c4f321b54c1f4821d56d19c40700518eb1
Nathan Rosa <nathan.wlgr@gmail.com>
2026-01-24 09:51:04 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
-import ErrorRecoverySystem from './components/ErrorRecoverySystem';
-import EntityNotFoundHandler from './components/EntityNotFoundHandler';
-import NetworkErrorBoundary from './components/NetworkErrorBoundary';
-import GlobalErrorInterceptor from './components/GlobalErrorInterceptor';
-import AIRateLimitProtection from './components/AIRateLimitProtection';
-
-import SystemHealthMonitor from './components/SystemHealthMonitor';
-import SecurityLayerSystem from './components/SecurityLayerSystem';
-import VoiceCommandAI from './components/VoiceCommandAI';
-import GoogleSheetsIntegration from './components/GoogleSheetsIntegration';
-import SystemHealthChecker from './components/SystemHealthChecker';
-import TestFlowSimulator from './components/TestFlowSimulator';
-import FollowUpAutomation from './components/FollowUpAutomation';
-import WhatsAppNotificationService from './components/WhatsAppNotificationService';
-import DocumentAIAnalyzer from './components/DocumentAIAnalyzer';
 
 export default function Layout({ children, currentPageName }) {
   return (
-    <ErrorRecoverySystem>
-      <GlobalErrorInterceptor />
-      <EntityNotFoundHandler>
-        <NetworkErrorBoundary>
-          <SystemHealthChecker />
-          <TestFlowSimulator />
-          <SystemHealthMonitor />
-          <SecurityLayerSystem />
-          <VoiceCommandAI />
-          <GoogleSheetsIntegration />
-          <FollowUpAutomation />
-                  <WhatsAppNotificationService />
-                  <AIRateLimitProtection />
-                  <div className="pb-20">
-                    {children}
-                  </div>
-                  <DocumentAIAnalyzer />
-        </NetworkErrorBoundary>
-      </EntityNotFoundHandler>
-    </ErrorRecoverySystem>
+    <div className="pb-20">
+      {children}
+    </div>
   );
 }
